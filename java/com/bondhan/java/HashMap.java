bbaf540877418de7438c83758e1a26bf42a27f41
bondhan <bondhan.novandy@gmail.com>
2024-01-14 19:31:34 +0700

    adding unit test

--- java/com/bondhan/java/HashMap.java
+++ java/com/bondhan/java/HashMap.java
@@ -1,6 +1,7 @@
 package com.bondhan.java;
 
 import java.io.*;
+import java.util.Objects;
 
 public class HashMap {
 
@@ -10,6 +11,10 @@ public class HashMap {
 
     public HashMap() {
         Nodes = new Node[SIZE];
+    }
+
+    public HashMap(int initial) {
+        Nodes = new Node[initial];
     }
 
     public int hash(String key) {
@@ -27,7 +32,88 @@ public class HashMap {
         return h & (SIZE - 1);
     }
 
-    public void Put(String key) {
+    public String Put(String key, String value) {
+        int index = getIndex(key);
 
+        Node n = new Node(key, value);
+
+        if (Nodes[index] == null) {
+            Nodes[index] = n;
+            return value;
+        }
+
+        Node curr = Nodes[index];
+
+        while (curr != null) {
+            if (Objects.equals(curr.Key, key)) {
+                curr.Value = value;
+                return value;
+            }
+
+            if (curr.Next == null) {
+                curr.Next = n;
+                return value;
+            }
+
+            curr = curr.Next;
+        }
+
+        return value;
+    }
+
+    public String Get(String key) {
+        int index = getIndex(key);
+
+        Node curr = Nodes[index];
+
+        while (curr != null) {
+
+            if (Objects.equals(curr.Key, key)) {
+                return curr.Value;
+            }
+
+            if (curr.Next == null) {
+                return "";
+            }
+
+            curr = curr.Next;
+        }
+
+        return "";
+    }
+
+    public String Remove(String key) {
+        int index = getIndex(key);
+
+        Node curr = Nodes[index];
+        Node prev = curr;
+
+        while (curr != null) {
+
+            if (Objects.equals(curr.Key, key)){
+                if (prev == curr) {
+                    String val = Nodes[index].Value;
+                    Nodes[index] = null;
+                    return val;
+                }
+
+                if (prev != curr) {
+                    if (curr.Next != null) {
+                        prev.Next = curr.Next;
+                        return curr.Value;
+                    }
+
+                    prev.Next = null;
+                    return curr.Value;
+                }
+            }
+
+            if (curr.Next != null) {
+                prev = curr;
+                curr = curr.Next;
+            }
+        }
+
+        return "";
     }
 }
--- java/com/bondhan/test/HashMapTest.java
+++ java/com/bondhan/test/HashMapTest.java
@@ -6,10 +6,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HashMapTest {
 
-    static HashMap hm;
+    HashMap hm;
 
-    @org.junit.jupiter.api.BeforeAll
-    static void setup() {
+    @org.junit.jupiter.api.BeforeEach
+    void setup() {
         hm = new HashMap();
     }
 
@@ -20,7 +20,7 @@ class HashMapTest {
     }
 
     @org.junit.jupiter.api.Test
-    void testGetIndex(){
+    void testGetIndex() {
         assertEquals(0, hm.getIndex(""));
         assertEquals(0x61 & (hm.SIZE - 1), hm.getIndex("a"));
         assertEquals(0x41 & (hm.SIZE - 1), hm.getIndex("A"));
@@ -28,6 +28,39 @@ class HashMapTest {
     }
 
     @org.junit.jupiter.api.Test
-    void put() {
+    void testPutGet() {
+        assertEquals(hm.Put("a", "a"), hm.Get("a"));
+        assertEquals(hm.Put("a", "b"), hm.Get("a"));
+        assertEquals(hm.Put("aB", "aB"), hm.Get("aB"));
+        assertEquals(hm.Put("aB", "aB"), hm.Get("aB"));
+
+        hm.Put("abba", "1");
+        hm.Put("abab", "2");
+        hm.Put("aabb", "3");
+
+        assertEquals("3", hm.Get("aabb"));
+        assertEquals("2", hm.Get("abab"));
+        assertEquals("1", hm.Get("abba"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void testRemove(){
+        hm.Put("a", "1");
+        String ret = hm.Remove("a");
+        assertEquals("1", ret);
+
+        assertEquals("", hm.Get("a"));
+
+        hm.Put("abba", "1");
+        hm.Put("abab", "2");
+        hm.Put("aabb", "3");
+
+        hm.Remove("aabb");
+        hm.Remove("abab");
+        hm.Remove("abba");
+
+        assertEquals("", hm.Get("abba"));
+        assertEquals("", hm.Get("abab"));
+        assertEquals("", hm.Get("aabb"));
     }
 }
